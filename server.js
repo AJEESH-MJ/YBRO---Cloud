42672505a01ec1d19c9b2ddb6656afef0bdd0e3b
AJEESH-MJ <ajeeshjayanm@gmail.com>
2023-10-31 22:12:31 +0530

    Database Connected

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
 
 // Configure env
 dotenv.config();
 
+// database config
+connectDB();
+
 // Create an Express app
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
 
 // Define a route
 app.get("/", (req, res) => {
